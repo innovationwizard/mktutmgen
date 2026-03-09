03a436ad3c9d4374cc7b772bd6048b9cc0134275
Jorge Luis Contreras Herrera <jorgeluiscontrerasherrera@gmail.com>
2026-03-09 11:15:42 -0600

    Middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,17 @@
-import { withAuth } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export default withAuth({
-  pages: {
-    signIn: "/login",
-  },
-});
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
